091fad05f3b791369a31ca1167340f7cd2261bbc
Namrata Chavan <109748579+namratachavan24@users.noreply.github.com>
2022-08-25 15:24:06 +0530

    UC-4 Added

---
+++ MobileNumber.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace UserRegistrationRegex
+{
+    public class MobileNumber
+    {
+
+        public static string Regex_MobileNumber = "^[1-9][0-9][ ]?[6-9][0-9]{9}$";
+
+        public bool validateMobileNumber(string mobileNumber)
+        {
+            return Regex.IsMatch(mobileNumber, Regex_MobileNumber);
+        }
+    }
+    
+}
