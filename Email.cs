1eafac6b3100a880c714c2189cbda400c9721bb8
Namrata Chavan <109748579+namratachavan24@users.noreply.github.com>
2022-08-25 15:23:21 +0530

    UC-3 Added

---
+++ Email.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace UserRegistrationRegex
+{
+    public class Email
+    {
+        public static string Regex_Email = "^[A-Za-z]{3,}([.][A-Za-z]{3,})?[@][A-Za-z]{2,}([.][A-Za-z]{2})?$";
+
+        public bool validateEmail(string email)
+        {
+            return Regex.IsMatch(email, Regex_Email);
+        }
+    }
+}
