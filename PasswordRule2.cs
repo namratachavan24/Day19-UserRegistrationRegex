e6b5c03e68534c13f91c6a9539d4ad5ed9084dd5
Namrata Chavan <109748579+namratachavan24@users.noreply.github.com>
2022-08-25 15:27:14 +0530

    UC-6 Added

---
+++ PasswordRule2.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace UserRegistrationRegex
+{
+    public class PasswordRule2
+    {
+        public static string Regex_Password = "(?=.*[A-Z])(?=.[0-9a-z]{7,})";
+
+        public bool validatePassword(string password)
+        {
+            return Regex.IsMatch(password, Regex_Password);
+        }
+    }
+}
