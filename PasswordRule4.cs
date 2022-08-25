8dfde21a14db7144e14def2b4252c6f4be1d48fb
Namrata Chavan <109748579+namratachavan24@users.noreply.github.com>
2022-08-25 15:28:03 +0530

    UC-8 Added

---
+++ PasswordRule4.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace UserRegistrationRegex
+{
+    public class PasswordRule4
+    {
+        public static string Regex_Password = "(?=.*[!@#$%^&*]{,1})(?=.[A-Za-z]{7,})";
+
+        public bool validatePassword(string password)
+        {
+            return Regex.IsMatch(password, Regex_Password);
+        }
+    }
+}
