43d2e77fb676b552c0319ec52ef0e5c64dc9bfcb
Namrata Chavan <109748579+namratachavan24@users.noreply.github.com>
2022-08-25 15:26:42 +0530

    UC-5 Added

---
+++ PasswordRule1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+
+namespace UserRegistrationRegex
+{
+    public class PasswordRule1
+    {
+        public static string Regex_Password = "^[a-zA-z0-9]{8,16}$";
+
+        public bool validatePassword(string password)
+        {
+            return Regex.IsMatch(password, Regex_Password);
+        }
+    }
+}
