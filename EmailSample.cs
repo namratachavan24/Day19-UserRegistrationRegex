cd05f9e2ff20651565301d580e67157047db0ee2
Namrata Chavan <109748579+namratachavan24@users.noreply.github.com>
2022-08-25 15:28:29 +0530

    UC-9 Added

---
+++ EmailSample.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace UserRegistrationRegex
+{
+    public class EmailSample
+    {
+        public static string Regex_Email = "^[a-z][a-z0-9]{2}([-.+]{1}[a-z0-9]{3})?[@][a-z0-9]{1,}[.][a-z]{3}([.][a-z]{2,3})?$";
+
+        public bool validateEmail(string email)
+        {
+            return Regex.IsMatch(email, Regex_Email);
+        }
+
+    }
+}
