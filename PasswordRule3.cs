a963573dfab6af04dbb8c41614f8eb561fb08a51
Namrata Chavan <109748579+namratachavan24@users.noreply.github.com>
2022-08-25 15:27:35 +0530

    UC-7 Added

---
+++ PasswordRule3.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace UserRegistrationRegex
+{
+    public class PasswordRule3
+    {
+        public static string Regex_Password = "(?=.*[0-9])(?=.[A-Za-z]{7,})";
+
+        public bool validatePassword(string password)
+        {
+            return Regex.IsMatch(password, Regex_Password);
+        }
+    }
+}
