4bc76b5533d715ff9135737f30c855b8f80ba716
Namrata Chavan <109748579+namratachavan24@users.noreply.github.com>
2022-08-25 15:22:14 +0530

    UC-2 Added

---
+++ LastName.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace UserRegistrationRegex
+{
+    public class LastName
+    {
+        public static string Regex_LastName = "^[A-Z][a-z]{2,}$";
+
+        public bool validateLastName(string lastName)
+        {
+            return Regex.IsMatch(lastName, Regex_LastName);
+        }
+    }
+}
